8d925931176c0b88529cd9dd78a03f81c8aa13ee
dzyls <stringnotnull@gmail.com>
2021-09-15 21:01:47 +0800

    feat(common):添加Notice接口，用于通知客户端

---
+++ chat-common/src/main/java/com/dzyls/chat/notice/Notice.java
@@ -0,0 +1,13 @@
+package com.dzyls.chat.notice;
+
+/**
+ * @Author <a href="stringnotnull@gmail.com">dzyls</a>
+ * @Date 2021/9/15 21:00
+ * @Version 1.0.0
+ * @Description:
+ */
+public interface Notice {
+
+    void noticeClient();
+
+}
---
+++ chat-common/src/main/java/com/dzyls/chat/notice/impl/NoticeImpl.java
@@ -0,0 +1,10 @@
+package com.dzyls.chat.notice.impl;
+
+/**
+ * @Author <a href="stringnotnull@gmail.com">dzyls</a>
+ * @Date 2021/9/15 21:01
+ * @Version 1.0.0
+ * @Description:
+ */
+public class NoticeImpl {
+}
