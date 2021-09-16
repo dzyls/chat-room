b700704108963791c197eef1a2a811681b8c0fb8
dzyls <stringnotnull@gmail.com>
2021-09-16 21:04:55 +0800

    feat(all):添加消息过期功能

--- chat-common/src/main/java/com/dzyls/chat/handler/MessageDispatcher.java
+++ chat-common/src/main/java/com/dzyls/chat/handler/MessageDispatcher.java
@@ -2,13 +2,14 @@ package com.dzyls.chat.handler;
 
 import com.dzyls.chat.annotate.HandlerOrder;
 import com.dzyls.chat.annotate.Server;
+import com.dzyls.chat.contants.ChatAttributeKey;
 import com.dzyls.chat.entity.CommonRequest;
 import com.dzyls.chat.entity.OperationType;
 import com.dzyls.chat.notify.Notice;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import io.netty.util.Attribute;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -33,7 +34,8 @@ public class MessageDispatcher extends SimpleChannelInboundHandler<CommonRequest
     protected void channelRead0(ChannelHandlerContext ctx, CommonRequest msg) throws Exception {
         OperationType operationType = msg.getOperationType();
         if (operationType == OperationType.MSG){
-            notice.noticeClient(msg.getMessage());
+            Attribute<Object> attr = ctx.channel().attr(ChatAttributeKey.CLIENT_NAME_KEY);
+            notice.noticeClient(msg.getMessage(), String.valueOf(attr.get()));
         }
         ctx.fireChannelRead(msg);
     }
