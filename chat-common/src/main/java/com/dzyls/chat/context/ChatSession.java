69a98d2248c048fa33197416fe5b6e77099043b6
dzyls <stringnotnull@gmail.com>
2022-03-03 22:38:03 +0800

    feat(common):添加ChatSession类

---
+++ chat-common/src/main/java/com/dzyls/chat/context/ChatSession.java
@@ -0,0 +1,18 @@
+package com.dzyls.chat.context;
+
+/**
+ * @Author <a href="stringnotnull@gmail.com">dzyls</a>
+ * @Date 2022/3/3 22:35
+ * @Version 1.0.0
+ * @Description: chat session
+ */
+public class ChatSession {
+
+    private long loginTime;
+
+    private long lastChatTime;
+
+
+
+
+}
